d61ccc31520d76e99117b7ffb516fec3f5ead0e0
Antoine LECOUSTRE <a.lecoustre@gmail.com>
2014-12-10 15:37:55 +0100

    Affichage des pions

--- graphique/index.php
+++ graphique/index.php
@@ -22,6 +22,25 @@
     </div>
 </nav>
 
+<legend style="top:13px;text-align:center;left:100px;right:100px"> CHRONOMETRE:
+    <div id="chrono" style="text-align:center">
+        <?php
+
+        function microtime_float()
+        {
+            list($usec, $sec) = explode(" ", microtime());
+            return ((float)$usec + (float)$sec);
+        }
+
+        $debut=0;
+        $fin  = microtime_float();
+        $time = $fin - $debut;
+        echo  $time." secondes\n";
+
+
+
+        ?></div></legend>
+
 <div class="container text-center">
     <div class="pagination-centered">
         <table border="1 solid">
@@ -34,22 +53,22 @@
                 ?>
             </tr>
             <?php
-                for($i=0;$i<5;$i++){ ?>
-                    <tr>
-                    <th><?php echo (5 -$i) ?></th>
+            for($i=0;$i<5;$i++){ ?>
+            <tr>
+                <th><?php echo $i ?></th>
 
-                    <td id="<?php echo (5 -$i) ?>a"></td>
-                    <td id="<?php echo (5 -$i) ?>b"></td>
-                    <td id="<?php echo (5 -$i) ?>c"></td>
-                    <td id="<?php echo (5 -$i) ?>d"></td>
-                    <td id="<?php echo (5 -$i) ?>e"></td>
+                <td id="<?php echo $i ?>0"></td>
+                <td id="<?php echo $i ?>1"></td>
+                <td id="<?php echo $i ?>2"></td>
+                <td id="<?php echo $i ?>3"></td>
+                <td id="<?php echo $i ?>4"></td>
 
 
-                    <?php
-                    echo "<th></th>";
-                    echo "</tr>";
+                <?php
+                echo "<th></th>";
+                echo "</tr>";
                 }
-            ?>
+                ?>
 
             <tr>
                 <th></th>
@@ -66,6 +85,49 @@
     </div>
 
 </div>
+
+
+<div id="player1" style="position:absolute;height:200px;width:100px;left:20px;top:140px;width: 200px">
+    <form name="form1">
+        <input  type="button" onclick="verif("form1")" name="j1" value="First Player" class=" btn-primary btn btn-striped" style="width: 200px"></div>
+<div id="nbpion1" style="position:absolute;height:200px;width:100px;left:20px;top:175px;width: 200px"
+<label style="font-size:10px">Pions restants :<input type="text" value="0" size="4" name="resultj1" style=" text-align:center;background-color:#A8CCEA;width: 200px"><br /></label></div>
+    <div id="nbpoins1" style="position:absolute;height:200px;width:100px;left:20px;top:220px;width: 200px"
+    <label style="font-size:10px">Points gagnes :<input type="text" value="0" size="4" name="resultj1" style=" text-align:center;background-color:#A8CCEA;width: 200px"></label></div>
+
+        </form>
+
+
+
+
+
+        <div id="player2" style="position:absolute;height:200px;width:100px;left:20px;top:280px">
+            <form name="form2">
+                <input type="button" onclick="verif("form2")" name="j2" value="Second Player"class=" btn-primary btn btn-striped" style="width: 200px"></div>
+
+        <div id="nbpion2" style="position:absolute;height:200px;width:100px;left:20px;top:315px;width: 200px"
+        <label style="font-size:10px">Pions restants :<input type="text" value="0" size="4" name="resultj2"style=" text-align:center;background-color:#A8CCEA;width: 200px"><br /></label></div>
+            <div id="nbpoins2" style="position:absolute;height:200px;width:100px;left:20px;top:360px;width: 200px"
+            <label style="font-size:10px">Points gagnes :<input type="text" value="0" size="4" name="resultj2"style=" text-align:center;background-color:#A8CCEA;width: 200px"></label></div>
+                </form>
+
+
+
+
+                <div id="adversaire" style="position:absolute;height:200px;width:250px;left:20px;top:440px">
+                    <fieldset>
+                        <legend> Vous voulez jouez contre: </legend>
+                        <form name="form3">
+                            <input type="radio" checked="" onclick="choix()" name="ba">
+                            Un autre joueur:
+                            <br>
+                            <br>
+                            <input type="radio" onclick="choix()" name="ba">
+                            Contre Moi??!! et je vais gagner :
+                        </form></div></fieldset>
+                <div>
+                    <button type="reset" onclick="demarrer()" name="restart" value="Recommencer?" class="btn btn-lg btn-primary btn-block" style="position:absolute;top:700px; width: 250px;left:640px;right:250">Recommencer ?</button></div>
+
 
 </body>
 </html>
--- graphique/js/event.js
+++ graphique/js/event.js
@@ -2,15 +2,180 @@
  * Created by antoine on 09/12/14.
  */
 $( document ).ready(function() {
-    var Jcourant = "b";
+    var tempMovePile = 0;
+
+    //test tableau case
+    var board = new Array(5);
+    for(var ligne=0; ligne<board.length;ligne++){
+        board[ligne] = new Array(5);
+
+        for(var colonne=0; colonne<board[ligne].length;colonne++){
+            board[ligne][colonne] = 0;
+        }
+
+
+    }
+
+    //test
+    board[0][0] = 0;
+    board[0][1] = 12;
+    board[0][2] = 121;
+    board[0][3] = 0;
+    board[0][4] = 1;
+
+    board[1][0] = 22;
+    board[1][1] = 111;
+    board[1][2] = 1222;
+    board[1][3] = 2;
+    board[1][4] = 0;
+
+    board[2][0] = 0;
+    board[2][1] = 0;
+    board[2][2] = 1;
+    board[2][3] = 21;
+    board[2][4] = 0;
+
+    board[3][0] = 0;
+    board[3][1] = 0;
+    board[3][2] = 0;
+    board[3][3] = 1;
+    board[3][4] = 0;
+
+    board[4][0] = 0;
+    board[4][1] = 0;
+    board[4][2] = 0;
+    board[4][3] = 0;
+    board[4][4] = 0;
+
+
+    var reponse;
+    $('#player1').click(function() {
+        var joueur1=1;
+        reponse =prompt("Quel est votre nom Joueur 1?");
+        alert (reponse);
+    });
+    $('#player2').click(function() {
+        var joueur2=2;
+        reponse = prompt("Quel est votre nom Joueur 2?");
+        alert (reponse);
+    });
+
 
     $("td").click(function() {
+        var idDiv = $(this).attr("id");
+
+        var ligne = idDiv.substring(0,1);
+        var colonne = idDiv.substring(1,2);
+
+        movePile(ligne,colonne);
+
+        if(tempMovePile !== 0){
+            if(board[ligne][colonne] === 0){
+                board[ligne][colonne] = tempMovePile;
+
+            }else{
+                board[ligne][colonne] = parseInt(String(tempMovePile) + String(board[ligne][colonne]));
+            }
+            console.log(tempMovePile);
+            tempMovePile = 0;
+        }
+
         if(Jcourant == "b"){
-            $(this).html('<div class="circleBlue"></div></td>');
+
+            //$(this).html('<div class="circleBlue"></div>');
             Jcourant = "r";
         }else{
-            $(this).html('<div class="circleRed"></div></td>');
+            //$(this).html('<div class="circleRed"></div>');
             Jcourant = "b";
         }
     });
+
+    $(window).load(function(){
+
+        placerPion(board);
+        //movePile(1,2);
+    });
+
+    var nbChiffre = function(number){
+        var ret = 1;
+        while(number >= 10){
+            ret += 1;
+            number = Math.floor(number / 10);
+        }
+
+        return ret;
+    };
+
+    var movePile = function(ligne, colonne){
+
+        if(board[ligne][colonne] !== 0){
+            var nbPions = prompt("Combien de pions voulez-vous déplacer ?","0");
+            var lengthValue = nbChiffre(board[ligne][colonne]);
+
+            var result = lengthValue - nbPions;
+            var diviseValue = board[ligne][colonne];
+            var charvalue = "";
+
+            for(var i=0;i<result;i++){
+                //charvalue = parseInt(String(charvalue) + String(diviseValue % 10));
+                charvalue = String(diviseValue % 10) + charvalue;
+                diviseValue = Math.floor(diviseValue/ 10);
+
+            }
+
+            tempMovePile = diviseValue;
+
+            board[ligne][colonne] = parseInt(charvalue);
+            console.log("pion restant" + board[ligne][colonne]);
+            //console.log(charvalue);
+        }
+    };
+
+    var placerPion = function(tab){
+
+        for(ligne=0;ligne<tab.length;ligne++){
+            for(colonne=0;colonne<tab[ligne].length;colonne++){
+                var sizeCircle = 85;
+                var valCase = tab[ligne][colonne];
+                var numberPion;
+                var nbPion = 0;
+                while(valCase != 0){
+                    numberPion = valCase % 10;
+
+
+                    //Création des cercles
+                    resizeCircle(String(ligne) +  String(colonne), nbPion, numberPion, sizeCircle);
+
+                    sizeCircle = sizeCircle - 20;
+
+                    valCase = Math.floor(valCase / 10);
+                    nbPion++;
+                }
+            }
+        }
+    };
+
+    var resizeCircle = function(id, nbPion, valuePion, sizeCircle){
+
+
+        if(valuePion === 1){
+            if(nbPion === 0){
+                $('#' + id).append('<div class="circleBlue" style="margin-top: -15px;"></div></td>');
+            }else{
+                $('#' + id).append('<div class="circleBlue" style="width: ' + sizeCircle + 'px; height: ' + sizeCircle + 'px; margin-top:' + String(-1 * sizeCircle - 5)  + 'px;"></div>');
+            }
+
+        }else{
+            if(nbPion === 0){
+                $('#' + id).append('<div class="circleRed" style="margin-top: -15px;"></div></td>');
+            }else{
+                $('#' + id).append('<div class="circleRed" style="width: ' + sizeCircle + 'px; height: ' + sizeCircle + 'px; margin-top:' + String(-1 * sizeCircle - 5)  + 'px;"></div>');
+            }
+
+            /*$('.circleRed').css({
+                width: sizeCircle + "px",
+                height: sizeCircle + "px"
+            });*/
+        }
+    };
 });
