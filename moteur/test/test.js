9ab3cab43296dbe4466dc9e5df423605ff12feeb
devin jonathan <jdevin@prune>
2014-12-10 11:26:48 +0100

    Ajout de Test Unitaire v1

--- moteur/test/test.js
+++ moteur/test/test.js
@@ -1,14 +1,36 @@
 /**
  * Created by jdevin on 09/12/14.
  */
-
+var motor = new moteur();
 MonTestCase=TestCase("Mon Test Case");
 MonTestCase.prototype.testA=function(){
-  var motor = new moteur();
     motor.init_plateau();
 
   assertTrue(motor.tab[0]=== 0);
 
-  assertTrue(motor.get_pos(2,2)=== 12);
-  assertTrue(motor.get_tete_pile(2,2)===0);
+
+  };
+
+MonTestCase.prototype.testB=function(){
+    assertTrue(motor.get_pos(2,2)=== 12);
 };
+
+MonTestCase.prototype.testC=function(){
+    assertTrue(motor.get_tete_pile(2,2)===0);
+};
+MonTestCase.prototype.testD=function(){
+    motor.place_marble(2,2);
+    assertTrue(motor.get_tete_pile(2,2)===1);
+};
+MonTestCase.prototype.testE=function(){
+    assertTrue(motor.ligne_droite(0,0,1,1)===true);
+};
+/*MonTestCase.prototype.testF=function(){
+    assertTrue(motor.distance_cases(2,3,1,2)===1);
+    assertTrue(motor.distance_cases(0,0,1,3)===2);
+};
+MonTestCase.prototype.testG=function(){
+    motor.place_marble(3,3);
+    assertTrue(motor.coup_possible(1,2,2,3,3)===true);
+};*/
+
